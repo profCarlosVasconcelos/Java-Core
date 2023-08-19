b354da58d9bc394662572d428ce5c637956a576c
Prof Carlos Vasconce <Prof Carlos Vasconce@192.168.66.61>
2023-08-18 21:54:50 -0300

    upload menu

--- Java-Core/src/util/CadastroMonitoramentoDespesas.java
+++ Java-Core/src/util/CadastroMonitoramentoDespesas.java
@@ -1,10 +1,31 @@
 package util;
 
+import java.util.Scanner;
+
+import entities.Menu;
+
 public class CadastroMonitoramentoDespesas {
-	String mensagem;
-	
-	public void mensage(String mensagem) {
-		this.mensagem = mensagem;
-		System.out.println();
+	public static void main(String[] args) {
+
+		Menu menu = new Menu("Menu de exemplo", new String[] { "1 - Cadastro de Saldo", "2 - Cadastro de Despesa", "3 - Visualizar Saldo" });
+		menu.show();
+
+		int option = menu.getOption(new Scanner(System.in));
+
+		switch (option) {
+		case 1:
+			System.out.println("1 - Cadastro de Saldo");
+			break;
+		case 2:
+			System.out.println("2 - Cadastro de Despesa");
+			break;
+		case 3:
+			System.out.println("3 - Visualizar Saldo");
+			break;
+		default:
+			System.out.println("4 - Opção inválida ");
+			break;
+		}
 	}
+
 }
